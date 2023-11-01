bfb1c6f1e1b28edbec081b82b7a80b239f4b6d95
Saleem Ibrahim <saleemibrahim673@gmail.com>
2023-11-01 02:03:23 -0400

    Update to Header.jsx
    Minor Update

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -12,28 +12,28 @@ const Header = () => {
 <div className=' relative w-full bg-transparent flex flex-col justify-between self-center'>
   
       <div className='flex justify-between px-4 py-3'>
-        <ImTerminal size={30}/>
+        <ImTerminal size={30} className='fill-white'/>
     <nav>
-      <ul className='flex space-x-4'>
-        <li className='hover:text-blue-400 cursor-pointer text-lg'>Home</li>
-        <Link href = "#About_Section" > <li className='hover:text-blue-400 cursor-pointer text-lg'>About</li></Link>
-        <Link   href = "#Experience_Section" className=' scroll-smooth'>  <li className='hover:text-blue-400 cursor-pointer text-lg'>Experience</li> </Link> 
-        <Link href = "#Projects_Section" className=' scroll-smooth'> <li className='hover:text-blue-400 cursor-pointer text-lg'>Projects</li> </Link> 
-        <Link href = "#Message_Form" className=' scroll-smooth'>  <li className='hover:text-blue-400 cursor-pointer text-lg'>Contact Me</li> </Link> 
+      <ul className=' flex space-x-4'>
+        <li className='hover:text-blue-400 tex-white  cursor-pointer text-lg'>Home</li>
+        <Link href = "#About_Section" > <li className='hover:text-blue-400 cursor-pointer text-lg text-white'>About</li></Link>
+        <Link   href = "#Experience_Section" className=' scroll-smooth'>  <li className='hover:text-blue-400 cursor-pointer text-lg text-white'>Experience</li> </Link> 
+        <Link href = "#Projects_Section" className=' scroll-smooth'> <li className='hover:text-blue-400 cursor-pointer text-lg text-white '>Projects</li> </Link> 
+        <Link href = "#Message_Form" className=' scroll-smooth'>  <li className='hover:text-blue-400 cursor-pointer text-lg text-white'>Contact Me</li> </Link> 
       </ul>
     </nav>
 
   </div>
   <div className='flex flex-col items-center mt-40 mb-14 justify-between'>
   <span className='text-blue-400 text-xl px-3'>Hi there!</span>
-  <span className='text-3xl mt-3'>
+  <span className='text-3xl mt-3 text-white'>
     My Name is <span className='text-blue-400'> Saleem Ibrahim</span>
 
   </span>
   <div className='flex space-x-4 justify-between'> 
   <ul className='flex space-x-4 mt-4'>
-  <Link href='https://github.com/saleemibrahim1212' target='_blank'> <li> <FaGithubAlt size={30}/></li></Link> 
-    <Link href="https://www.linkedin.com/in/saleem-ibrahim-0162a8121/" target='_blank' >  <li> <AiFillLinkedin size={30}/></li></Link>
+  <Link href='https://github.com/saleemibrahim1212' target='_blank'> <li> <FaGithubAlt size={30} className="fill-white"/></li></Link> 
+    <Link href="https://www.linkedin.com/in/saleem-ibrahim-0162a8121/" target='_blank' >  <li> <AiFillLinkedin size={30} className='fill-white'/></li></Link>
     </ul>
     </div>
   <div className='flex p-6 rounded-b-md '>
@@ -42,7 +42,7 @@ const Header = () => {
   </div>
 </div>
 <div className='flex flex-auto mt-64'> 
-<Link href='#About_Section' className='font-sans ml-3 mb-10'> Scroll down</Link>
+<Link href='#About_Section' className='font-sans ml-3 text-white mb-10'> Scroll down</Link>
 <Link href='#About_Section' className='text-blue-400 ml-1 mt-2 hover: cursor-pointer'><AiOutlineDown/>   </Link>
 </div>
 </div>
